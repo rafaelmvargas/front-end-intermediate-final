5309a96224d2ad269efba900bdd814bdc8b7c5db
Rafael Vargas <rafael.vargas@outlook.com>
2021-11-30 20:54:34 -0500

    added custom hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [recipes, setRecipes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`/api/recipes`)
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Recipes recipes={recipes} />} />
+          <Route
+            path="/:recipeId"
+            element={<RecipeDetail recipes={recipes} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
-
 export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import useToggle from "./hooks/useToggle";
+
+function Toggler() {
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isBanana, toggleIsBanana] = useToggle(true);
+
+  return (
+    <div>
+      <h1 onClick={toggleIsHappy}>{isHappy ? "😄" : "😢"}</h1>
+      <h1 onClick={toggleIsBanana}>{isBanana ? "🍌" : "👹"}</h1>
+    </div>
+  );
+}
+
+export default Toggler;
